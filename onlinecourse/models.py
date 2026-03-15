3e5c0a13795de9a1eca384b6515a87bb77280b4a
Piyush-jangid-01 <jangidpiyush16011@gmail.com>
2026-03-16 00:24:10 +0530

    fix course_detail template

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -101,6 +101,7 @@ class Enrollment(models.Model):
 #class Submission(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
+
 class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
